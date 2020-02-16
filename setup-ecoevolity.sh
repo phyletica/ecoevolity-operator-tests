137bf4b2289ad548b0523bd6ef33b0f50df88807
Jamie R. Oaks <joaks1@gmail.com>
2020-02-15 23:22:40 -0600

    Adding shell script to setup ecoevolity.

---
+++ setup-ecoevolity.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+set -e
+
+ecoevolity_commit="fb4c09169"
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    module load gcc/5.3.0
+    module load cmake/3.7.1
+fi
+
+project_dir=""
+this_dir=`dirname "$0"`
+if [ "$this_dir" = "." ]
+then
+    project_dir="$(pwd)"
+else
+    cd "$this_dir"
+    project_dir="$(pwd)"
+fi
+
+git clone https://github.com/phyletica/ecoevolity.git
+cd ecoevolity
+git checkout -b testing "$ecoevolity_commit"
+./build.sh --prefix "$project_dir"
+cd ..
+rm -r ecoevolity
