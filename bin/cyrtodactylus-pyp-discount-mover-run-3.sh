4adce5abbff53f0e427e8c8b853f6611bb9f3943
Jamie R. Oaks <joaks1@gmail.com>
2020-02-15 23:23:33 -0600

    Adding qsub scripts.

---
+++ bin/cyrtodactylus-pyp-discount-mover-run-3.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    module load gcc/5.3.0
+fi
+
+seed=971685878
+run_number=3
+output_dir="../ecoevolity-output"
+
+if [ ! -d "$output_dir" ] 
+then
+    mkdir -p "$output_dir"
+fi
+
+prefix="${output_dir}/run-${run_number}-"
+config_name="cyrtodactylus-pyp-discount-mover"
+config_path="../configs/${config_name}.yml"
+std_out_path="${prefix}${config_name}.out"
+
+./ecoevolity --seed "$seed" --prefix "$prefix" --relax-missing-sites --relax-constant-sites --relax-triallelic-sites "$config_path" 1>"$std_out_path" 2>&1
